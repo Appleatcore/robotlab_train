e8d604ded63680fa06bf251a040f2fd17ccb14ae
appleatcore <13599380323@163.com>
2026-05-26 20:17:03 +0800

    feat:将高程图的信息归一化后再输入到网络中训练

--- source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/mdp/observations.py
+++ source/robot_lab/robot_lab/tasks/manager_based/locomotion/velocity/mdp/observations.py
@@ -9,6 +9,7 @@ import torch
 
 from isaaclab.assets import Articulation
 from isaaclab.managers import SceneEntityCfg
+from isaaclab.sensors import RayCaster
 
 if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedEnv, ManagerBasedRLEnv
@@ -33,3 +34,16 @@ def phase(env: ManagerBasedRLEnv, cycle_time: float) -> torch.Tensor:
     phase = env.episode_length_buf[:, None] * env.step_dt / cycle_time
     phase_tensor = torch.cat([torch.sin(2 * torch.pi * phase), torch.cos(2 * torch.pi * phase)], dim=-1)
     return phase_tensor
+
+
+def height_scan_norm(env: ManagerBasedEnv, sensor_cfg: SceneEntityCfg, offset: float = 0.5) -> torch.Tensor:
+    """Height scan from the given sensor w.r.t. the sensor's frame.
+
+    The provided offset (Defaults to 0.5) is subtracted from the returned values.
+    """
+    # extract the used quantities (to enable type-hinting)
+    sensor: RayCaster = env.scene.sensors[sensor_cfg.name]
+    # height scan: height = sensor_height - hit_point_z - offset
+    height_scan_raw = sensor.data.pos_w[:, 2].unsqueeze(1) - sensor.data.ray_hits_w[..., 2] - offset
+    height_scan = (height_scan_raw - torch.mean(height_scan_raw)) / torch.std(height_scan_raw)
+    return height_scan
